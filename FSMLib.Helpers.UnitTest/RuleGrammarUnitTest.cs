3e1da9cf716b2e611b73bfe7caa621da7bc132ef
dfgs <dfgs83@gmail.com>
2019-05-15 10:11:44 +0200

    Improved rule parser

---
+++ FSMLib.Helpers.UnitTest/RuleGrammarUnitTest.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FSMLib.Helpers;
+using Sprache;
+using FSMLib.Predicates;
+
+namespace FSMLib.Helpers.UnitTest
+{
+	[TestClass]
+	public class RuleGrammarUnitTest
+	{
+		
+
+
+		[TestMethod]
+		public void ShouldParseNonTerminal()
+		{
+			NonTerminal<char> result;
+			result = RuleGrammar.NonTerminal.Parse("<test>");
+			Assert.AreEqual("test", result.Name);
+		}
+
+		[TestMethod]
+		public void ShouldParseTerminal()
+		{
+			Terminal<char> result;
+			result = RuleGrammar.Terminal.Parse("a");
+			Assert.AreEqual('a', result.Value);
+			result = RuleGrammar.Terminal.Parse("1");
+			Assert.AreEqual('1', result.Value);
+			result = RuleGrammar.Terminal.Parse("-");
+			Assert.AreEqual('-', result.Value);
+		}
+		[TestMethod]
+		public void ShouldParseTerminalFromEscapedChar()
+		{
+			Terminal<char> result;
+			result = RuleGrammar.Terminal.Parse(@"\a");
+			Assert.AreEqual('a', result.Value);
+			result = RuleGrammar.Terminal.Parse(@"\<");
+			Assert.AreEqual('<', result.Value);
+			result = RuleGrammar.Terminal.Parse(@"\>");
+			Assert.AreEqual('>', result.Value);
+			result = RuleGrammar.Terminal.Parse(@"\.");
+			Assert.AreEqual('.', result.Value);
+			result = RuleGrammar.Terminal.Parse(@"\*");
+			Assert.AreEqual('*', result.Value);
+			result = RuleGrammar.Terminal.Parse(@"\?");
+			Assert.AreEqual('?', result.Value);
+		}
+		[TestMethod]
+		public void ShouldNotParseSpecialCharactersAsTerminal()
+		{
+			Assert.ThrowsException<ParseException>(() => RuleGrammar.Terminal.Parse("<"));
+			Assert.ThrowsException<ParseException>(() => RuleGrammar.Terminal.Parse(">"));
+			Assert.ThrowsException<ParseException>(() => RuleGrammar.Terminal.Parse(@"\"));
+			Assert.ThrowsException<ParseException>(() => RuleGrammar.Terminal.Parse("."));
+			Assert.ThrowsException<ParseException>(() => RuleGrammar.Terminal.Parse("*"));
+			Assert.ThrowsException<ParseException>(() => RuleGrammar.Terminal.Parse("?"));
+		}
+		[TestMethod]
+		public void ShouldParseAnyTerminal()
+		{
+			AnyTerminal<char> result;
+			result = RuleGrammar.AnyTerminal.Parse(".");
+			Assert.IsNotNull(result);
+		}
+		[TestMethod]
+		public void ShouldParseTerminalSequence()
+		{
+			Sequence<char> result;
+
+			result = RuleGrammar.TerminalSequence.Parse("abcd");
+			Assert.AreEqual(4,result.Items.Count);
+			Assert.IsInstanceOfType(result.Items[0], typeof(Terminal<char>));
+			Assert.IsInstanceOfType(result.Items[1], typeof(Terminal<char>));
+			Assert.IsInstanceOfType(result.Items[2], typeof(Terminal<char>));
+			Assert.IsInstanceOfType(result.Items[3], typeof(Terminal<char>));
+
+			result = RuleGrammar.TerminalSequence.Parse(@"ab\.d");
+			Assert.AreEqual(4, result.Items.Count);
+			Assert.IsInstanceOfType(result.Items[0], typeof(Terminal<char>));
+			Assert.IsInstanceOfType(result.Items[1], typeof(Terminal<char>));
+			Assert.IsInstanceOfType(result.Items[2], typeof(Terminal<char>));
+			Assert.IsInstanceOfType(result.Items[3], typeof(Terminal<char>));
+
+			result = RuleGrammar.TerminalSequence.Parse(@"ab.d");
+			Assert.AreEqual(4, result.Items.Count);
+			Assert.IsInstanceOfType(result.Items[0], typeof(Terminal<char>));
+			Assert.IsInstanceOfType(result.Items[1], typeof(Terminal<char>));
+			Assert.IsInstanceOfType(result.Items[2], typeof(AnyTerminal<char>));
+			Assert.IsInstanceOfType(result.Items[3], typeof(Terminal<char>));
+		}
+		
+
+		[TestMethod]
+		public void ShouldParseOneOrMore()
+		{
+			OneOrMore<char> result;
+			result = RuleGrammar.OneOrMore.Parse("a+");
+			Assert.IsInstanceOfType(result.Item, typeof(Terminal<char>));
+			result = RuleGrammar.OneOrMore.Parse(".+");
+			Assert.IsInstanceOfType(result.Item, typeof(AnyTerminal<char>));
+		}
+		[TestMethod]
+		public void ShouldParseZeroOrMore()
+		{
+			ZeroOrMore<char> result;
+			result = RuleGrammar.ZeroOrMore.Parse("a*");
+			Assert.IsInstanceOfType(result.Item, typeof(Terminal<char>));
+			result = RuleGrammar.ZeroOrMore.Parse(".*");
+			Assert.IsInstanceOfType(result.Item, typeof(AnyTerminal<char>));
+		}
+		[TestMethod]
+		public void ShouldParseOptional()
+		{
+			Optional<char> result;
+			result = RuleGrammar.Optional.Parse("a?");
+			Assert.IsInstanceOfType(result.Item, typeof(Terminal<char>));
+			result = RuleGrammar.Optional.Parse(".?");
+			Assert.IsInstanceOfType(result.Item, typeof(AnyTerminal<char>));
+		}
+
+
+		[TestMethod]
+		public void ShouldParseRulePredicate()
+		{
+			BasePredicate<char> result;
+
+			result = RuleGrammar.RulePredicate.Parse("a");
+			Assert.IsInstanceOfType(result, typeof(Terminal<char>));
+			result = RuleGrammar.RulePredicate.Parse("a?");
+			Assert.IsInstanceOfType(result, typeof(Optional<char>));
+			result = RuleGrammar.RulePredicate.Parse("abcd");
+			Assert.IsInstanceOfType(result, typeof(Sequence<char>));
+
+
+		}
+
+
+	}
+}
---
+++ FSMLib.Helpers/RuleGrammar.cs
@@ -0,0 +1,86 @@
+﻿using FSMLib.Predicates;
+using Sprache;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSMLib.Helpers
+{
+    public class RuleGrammar
+    {
+		private static readonly Parser<char> OpenBracket = Parse.Char('<');
+		private static readonly Parser<char> CloseBracket = Parse.Char('>');
+		private static readonly Parser<char> BackSlash = Parse.Char('\\');
+		private static readonly Parser<char> Dot = Parse.Char('.');
+		private static readonly Parser<char> Plus = Parse.Char('+');
+		private static readonly Parser<char> Star = Parse.Char('*');
+		private static readonly Parser<char> QuestionMark = Parse.Char('?');
+
+		private static Parser<char> SpecialChar = OpenBracket.Or(CloseBracket).Or(BackSlash).Or(Dot).Or(Plus).Or(Star).Or(QuestionMark);
+		private static Parser<char> NormalChar = Parse.AnyChar.Except(SpecialChar);
+
+		private static readonly Parser<char> EscapedChar =
+			from _ in BackSlash
+			from c in Parse.AnyChar
+			select c;
+
+
+		public static readonly Parser<Terminal<char>> Terminal =
+			from value in NormalChar.Or(EscapedChar)
+			select new Terminal<char>() {Value = value };
+
+		public static readonly Parser<NonTerminal<char>> NonTerminal =
+			from open in OpenBracket
+			from name in Parse.AnyChar.Except(CloseBracket).Many().Text().Token()
+			from close in CloseBracket
+			select new NonTerminal<char>() { Name = name };
+
+		public static readonly Parser<AnyTerminal<char>> AnyTerminal =
+			from _ in Dot
+			select new AnyTerminal<char>();
+
+		private static readonly Parser<BasePredicate<char>> TerminalPredicate =
+			Terminal.Or<BasePredicate<char>>(NonTerminal)
+			.Or<BasePredicate<char>>(AnyTerminal);
+
+		public static readonly Parser<Sequence<char>> TerminalSequence =
+			from firstPredicate in TerminalPredicate
+			from otherPredicates in TerminalPredicate.AtLeastOnce()
+			select (Sequence<char>)(new BasePredicate<char>[] { firstPredicate }.Concat(otherPredicates) ).ToArray();
+
+
+		public static readonly Parser<OneOrMore<char>> OneOrMore =
+			from value in TerminalPredicate
+			from _ in Plus
+			select new OneOrMore<char>() { Item = value };
+
+		public static readonly Parser<ZeroOrMore<char>> ZeroOrMore =
+			from value in TerminalPredicate
+			from _ in Star
+			select new ZeroOrMore<char>() { Item = value };
+
+		public static readonly Parser<Optional<char>> Optional =
+			from value in TerminalPredicate
+			from _ in QuestionMark
+			select new Optional<char>() { Item = value };
+
+		private static readonly Parser<BasePredicate<char>> AdvancedPredicate =
+			TerminalSequence.Or<BasePredicate<char>>(OneOrMore)
+			.Or<BasePredicate<char>>(ZeroOrMore).Or(Optional);
+
+		private static readonly Parser<BasePredicate<char>> RuleSinglePredicate =
+			from predicate in AdvancedPredicate.Or(TerminalPredicate)
+			select predicate;
+
+		private static readonly Parser<Sequence<char>> RuleSequencePredicate =
+			from predicates in (AdvancedPredicate.Or(TerminalPredicate)).Many()
+			select (Sequence<char>)predicates.ToArray();
+
+		public static readonly Parser<BasePredicate<char>> RulePredicate =
+			from predicate in RuleSinglePredicate.Or(RuleSequencePredicate)
+			select predicate;
+
+	}
+}
